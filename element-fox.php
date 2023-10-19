25daf74ab1cd880d07393a1b0b50a00886f0a4e5
ogjoshbic <148372309+ogjoshbic@users.noreply.github.com>
2023-10-19 00:28:27 -0500

    Created Wrapper

---
+++ element-fox.php
@@ -0,0 +1,8 @@
+/*
+ * Plugin Name:       Element Fox
+ * Description:       Adds an after submit action to Elementor Pro Form that links to a Sendfox email list.
+ * Version:           1
+ * Requires at least: 5.2
+ * Requires PHP:      8.2
+ * Author:            Josh Bickford
+ */
